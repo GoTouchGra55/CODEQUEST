34a1b02efc20598bb8574a918e0d072391a2efa0
GoTouchGra55 <shauryatamang.dev@gmail.com>
2026-09-15 11:33:12 +0545

    Add basic websocket features

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller.js';
 import { AppService } from './app.service.js';
+import { LobbyGateway } from './lobby/lobby.gateway.js';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, LobbyGateway],
 })
 export class AppModule {}
